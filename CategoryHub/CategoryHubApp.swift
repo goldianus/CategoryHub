1d94de2b80cd48e05396b897442c8bf594b95f10
goldy <goldianus.mbete@erajaya.com>
2026-08-22 21:37:10 +0700

    Initial Commit

---
+++ CategoryHub/CategoryHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryHubApp.swift
+//  CategoryHub
+//
+//  Created by Goldianus SM on 22/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CategoryHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
